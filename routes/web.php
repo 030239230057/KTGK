744bc0179aee2e332f2ed878c5b27bd39524406f
ltphuonganh <030239230007@st.buh.edu.vn>
2026-04-07 08:32:43 +0700

    câu 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController3; 
+
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+Route::get('/movie/create', [MovieController3::class, 'create'])->name('movie.create');
+Route::post('/movie/store', [MovieController3::class, 'store'])->name('movie.store');
