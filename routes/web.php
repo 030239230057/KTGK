8d86fe7e40569cb4c2abaac479e8f5b42c5b87da
Thu Hoai <030239230057@st.buh.edu.vn>
2026-04-07 10:10:14 +0700

    cập nhật

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-Route::get('/', [App\Http\Controllers\MovieController1::class, 'index']);
-Route::get('/theloai/{id}', [App\Http\Controllers\MovieController1::class, 'getByGenre']);
-Route::post('/timkiem', [App\Http\Controllers\MovieController1::class, 'search']);
-Route::get('/chitiet/{id}', [App\Http\Controllers\MovieController1::class, 'detail']);
 use App\Http\Controllers\MovieController;
-Route::get('/', function () {
-    return view('welcome'); 
-});
+use App\Http\Controllers\MovieController1;
+
+
+
+Route::get('/trangchu', [MovieController1::class, 'index']); 
+
+Route::get('/theloai/{id}', [MovieController1::class, 'getByGenre']);
+Route::post('/timkiem', [MovieController1::class, 'search']);
+Route::get('/chitiet/{id}', [MovieController1::class, 'detail']);
+
+
 Route::get('/admin/movies', [MovieController::class, 'trangQuanLy'])->name('admin.movies');
 
-// 3. Chức năng Xóa phim
 Route::get('/admin/movies/delete/{id}', [MovieController::class, 'xoaMemPhim'])->name('admin.movies.delete');
-
